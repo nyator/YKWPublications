7a211d87dff1341c9cf712fe93ddff8dc9131db6
Henry <hhen5562@gmail.com>
2024-05-09 03:06:51 +0000

    cont...adds

--- ykwp/src/main.jsx
+++ ykwp/src/main.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Home from '../src/pages/Home.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/Home.jsx";
+import Publications from "./pages/Publications.jsx";
+import AboutUs from "./pages/AboutUs.jsx";
+import Gallery from "./pages/Gallery.jsx";
+import Team from "./pages/Team.jsx";
+import NotFound from "./pages/NotFound.jsx";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: "/",
+    element: <Publications />,
+  },
+  {
+    path: "/",
+    element: <AboutUs />,
+  },
+  {
+    path: "/",
+    element: <Gallery />,
+  },
+  {
+    path: "/",
+    element: <Team />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
